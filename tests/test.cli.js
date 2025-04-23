111733b0cc59f0524249e0404e778c10504d3716
Dhiraj <dkskp2005@gmail.com>
2025-04-23 21:32:08 +0530

    test cli with args

---
+++ tests/test.cli.js
@@ -0,0 +1,29 @@
+import { argv } from "node:process";
+import chalk from "chalk";
+
+async function runTests() {
+  const testType = argv[2];
+
+  try {
+    switch (testType) {
+      case "lexer":
+        console.log(chalk.blueBright("Running lexer tests..."));
+        await import("./lexer.test.js");
+        break;
+      case "parser":
+        console.log(chalk.blueBright("Running parser tests..."));
+        // await import("./parser.test.js");
+        break;
+      default:
+        console.log(chalk.yellow("Usage: node cli.js <lexer|parser>"));
+        process.exit(1);
+    }
+
+    console.log(chalk.greenBright(`✅ ${testType} tests passed successfully!`));
+  } catch (err) {
+    console.error(chalk.red(`❌ Error running ${testType} tests:\n`), err);
+    process.exit(1);
+  }
+}
+
+runTests();
